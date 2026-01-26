4e91382d5be79059660d06d11efb2736463f2648
Achille Rivoltella <achille.rivoltella@gmail.com>
2026-01-26 17:09:16 +0100

    latest version plot quality

--- quality/01_PlotQuality.R
+++ quality/01_PlotQuality.R
@@ -10,90 +10,83 @@ library(hrbrthemes)
 library(viridis)
 #library(related)
 
-args <- commandArgs(trailingOnly = TRUE)
-
 ## Read the VCF
+VCF1 = read.vcfR("../Achille/data/sample_SNP.vcf.gz")
 
-VCF1=read.vcfR(paste0("VCFNewNamesOldGenome/",args[2],"_NewGenome_",args[1],"_GATK_TAG_Flowqual_Noindels_Norepeat_SNP.vcf.gz"))
+DP1 <- extract.gt(VCF1, element='DP', as.numeric = TRUE) 
+DP_melt1 = melt(DP1)    # Pour avoir les noms d'individus 
 
-DP1<- extract.gt(VCF1, element='DP', as.numeric = TRUE) 
-DP_melt1=melt(DP1)
-geno1<-extract.gt(VCF1,element="GT",mask=F,as.numeric=F,return.alleles = F,
-                 convertNA = F,extract = T)
-geno_table1=as.data.frame(geno1)
+geno1 <- as.data.frame(extract.gt(VCF1,element="GT", mask=F,as.numeric=F,return.alleles = F,
+                 convertNA = F,extract = T))
+# Génotype à chaque position pour chaque ind.
 
-#Coverage Information
 
-moyenne_cover_par_indiv1 = apply(DP1,2,mean,na.rm=T)
-moyenne_cover_par_Ind_table1=as.data.frame(moyenne_cover_par_indiv1)
+#### Populations #### -------------------------------------------------------------------
+# Correspondance individus - population : 
 
-#Mean Per Line
+names_ind <- colnames(DP1)
+names_ind <- as_tibble(names_ind)
+names_ind <- names_ind |>
+  mutate(pop = str_extract(colnames(DP1), "^[A-Za-z]+|^[0-9]+")) |>
+  mutate(pop = ifelse(pop == "A", "N_ATL",
+               ifelse(pop == "Ber", "BERING", 
+               ifelse(pop == "Chi", "CHILI", 
+               ifelse(pop == "Kar", "KARAGINSKY", 
+               ifelse(pop == "Pe", "PEROU", "MADAGASCAR"))))))
 
-moyenne_cover_par_pos1= apply(DP1,1,mean,na.rm=T)
-moyenne_cover_par_pos_table1=as.data.frame(moyenne_cover_par_pos1)
+names_ind |>
+  summarise(n = n(), 
+            .by = pop) |>
+  arrange(desc(n))
 
-#Position vector
 
-position_vector1=as.numeric(str_remove(rownames(DP1),paste0(args[1],"_")))
-moyenne_cover_par_pos_table_pos1=cbind(moyenne_cover_par_pos_table1, position_vector1)
+#### POSITION #### ----------------------------------------------------------------------
+summary_position = data.frame(depth_pos = apply(DP1, 1, mean,na.rm=T))
+position <- as.numeric(str_remove(rownames(summary_position), "1_"))
 
-#Row count of Genotype NA
+summary_position <- summary_position |>
+  mutate(position = as.numeric(str_remove(rownames(summary_position), "1_")),  # Position du nucléotide 
+         sum_NA = rowSums(geno1 == "./."),                         # Somme des NA / position
+         prop_NA_ind = (sum_NA/dim(geno1)[2])*100,                 # Proportion des NA / individu
+         het_pos = rowSums(geno1 == "0/1"),                        # Nb d'Hz / position
+         prop_het = (het_pos/dim(geno1)[2])*100)                   # Proportion d'Hz / individu (%)
+head(summary_position)
 
-sum_pos_geno_table1 = as.data.frame(rowSums(geno1=="./.")) # put "./.", "0/0", "0/1" or "1/1" depending on the genotype you want to count
 
-# By individuals
 
-sum_ind_geno_table1 = as.data.frame(colSums(geno1=="./."))
 
-# By position
+#### INDIVIDUS #### ---------------------------------------------------------------------
+summary_individuals = data.frame(depth_ind = apply(DP1, 2, mean, na.rm = T))
 
-Prop_Pos_geno1=((sum_pos_geno_table1/dim(geno_table1)[2])*100)
-Prop_Pos_geno_table1=as.data.frame(Prop_Pos_geno1)
-position_vector1=as.numeric(str_remove(rownames(Prop_Pos_geno_table1),paste0(args[1],"_")))
-Prop_Pos_geno_table_pos1=cbind(Prop_Pos_geno_table1, position_vector1)
+summary_individuals <- summary_individuals |>
+  mutate(ind = rownames(summary_individuals),                # Noms des individus 
+         pop = names_ind$pop,                                # Population d'appartenance
+         sum_NA = colSums(geno1=="./."),                     # Somme des NA / individu
+         prop_NA_pos = ((sum_NA/dim(geno1)[1])*100),         # Proportion de NA / position
+         het_ind = colSums(geno1=="0/1"),                    # Nb d'Hz / individu
+         prop_het_pos = ((het_ind/dim(geno1)[1])*100))       # Proportion d'Hz / position
+head(summary_individuals)
 
-# By individuals
 
-Prop_Ind_geno1=((sum_ind_geno_table1/dim(geno_table1)[1])*100)
-Prop_Ind_geno_table1=as.data.frame(Prop_Ind_geno1)
 
-#Row count of Het
 
-sum_pos_geno_table1_Het = as.data.frame(rowSums(geno1=="0/1")) # put "./.", "0/0", "0/1" or "1/1" depending on the genotype you want to count
-
-# By individuals
-
-sum_ind_geno_table1_Het = as.data.frame(colSums(geno1=="0/1"))
-
-# By position
-
-Prop_Pos_geno1_Het=((sum_pos_geno_table1_Het/dim(geno_table1)[2])*100)
-Prop_Pos_geno_table1_Het=as.data.frame(Prop_Pos_geno1_Het)
-Prop_Pos_geno_table_pos1_Het=cbind(Prop_Pos_geno_table1_Het, position_vector1)
-
-# By individuals
-Prop_Ind_geno1_Het=((sum_ind_geno_table1_Het/dim(geno_table1)[1])*100)
-Prop_Ind_geno_table1_Het=as.data.frame(Prop_Ind_geno1_Het)
-
-## Plots of VCF before filtering
+#### Plots of VCF before filtering #### -------------------------------------------------
 
 #Depth distrib
-
-g0=ggplot()+
-  geom_histogram(data=moyenne_cover_par_pos_table_pos1,
-		 aes(x=moyenne_cover_par_pos_table_pos1$moyenne_cover_par_pos1),
+g0 <- summary_position |>
+  ggplot()+
+  geom_histogram(aes(x = depth_pos),
 		 binwidth = 4, fill = "steelblue", color = "black") +
   scale_x_continuous(limits = c(0, 200), breaks = seq(0, 100, by = 10)) +
-  labs(x =paste0("Distribition of average depth per site (",args[2]," Chrom ",args[1],")"),
+  labs(x = "Distribition of average depth per site",
        y = "Count")
-# Depth per site
 
-g1=ggplot()+
-  geom_point(data=moyenne_cover_par_pos_table_pos1, 
-             aes(y=moyenne_cover_par_pos_table_pos1$moyenne_cover_par_pos1,
-                 x=moyenne_cover_par_pos_table_pos1$position_vector1),
+
+# Depth per site
+g1 <- summary_position |>
+  ggplot()+
+  geom_point(aes(y = depth_pos, x = position),
              color="black", pch=20, size=1) +
-  
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
   labs(y="mean depth", x = "PB")+
   ggtitle("Average depth sequencing of SNPs DP1")+
@@ -101,78 +94,87 @@ g1=ggplot()+
   # vizualise only range of sequencing depth detph
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100))
 
-# Depth per Individual
 
-g2=ggplot()+
-geom_boxplot(data=DP_melt1, aes(x=DP_melt1$Var2, 
-                               y=DP_melt1$value,
-                                 colour="purple"),
-               color="black") +
-  theme(axis.title.x=element_blank())+
-  labs(y="SNP sequencing depth", x = "samples")+
+# Depth per Individual
+g2 <- summary_individuals |>
+  ggplot() + 
+  geom_boxplot(aes(x = ind, y = depth_ind, color= pop)) +
+  theme(axis.title.x = element_blank())+
+  labs(y="SNP sequencing depth", x = "samples") +
   ggtitle("depth sequencing distribution of SNP filtered for qualtity and low depth") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
   # vizualise only range of sequencing depth detph
   scale_y_continuous(breaks = seq(0, 260, by = 20), limits = c(0, 260))
 
-# NA per Position
 
-g3=ggplot()+
-  geom_point(data=Prop_Pos_geno_table_pos1, 
-             aes(y=Prop_Pos_geno_table_pos1$`rowSums(geno1 == "./.")` , 
-                 x=Prop_Pos_geno_table_pos1$position_vector1), 
-             color="purple",
-             size=1)+
+# NA per Position
+g3 <- summary_position |>
+  ggplot() +
+  geom_point(aes(y = sum_NA, x = position), color="purple", size = 1)+
   labs(x="pos", y = "Prop './.' site") +
   ggtitle("Prop './.' site by position") +
-  scale_x_continuous(breaks = seq(0, position_vector1[length(position_vector1)], 
+  scale_x_continuous(breaks = seq(0, position[length(position)], 
                                   by = 1000000)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
-# NA per Individual
 
-g4=ggplot()+
-  geom_point(data=Prop_Ind_geno_table1, 
-             aes(y=Prop_Ind_geno_table1$`colSums(geno1 == "./.")`, 
-                 x=row.names(Prop_Ind_geno_table1)),
-             color="purple",
-             size=2) +
+# NA per Individual
+g4 <- summary_individuals |>
+  ggplot()+
+  geom_point(aes(y = sum_NA, x = ind), color="purple", size=2) +
   labs(x="Ind", y = "Prop './.' site") +
   ggtitle("Prop './.' site by individuals") + 
   theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100))
 
-# Het per Position
 
-g5=ggplot()+
-  geom_point(data=Prop_Pos_geno_table_pos1_Het, 
-             aes(y=Prop_Pos_geno_table_pos1_Het$`rowSums(geno1 == "0/1")` , 
-                 x=Prop_Pos_geno_table_pos1_Het$position_vector1), 
-             color="purple",
-             size=1)+
+# Het per Position
+g5 <- summary_position |>
+  ggplot() +
+  geom_point(aes(y = het_pos , x= position), color = "purple", size = 0.3)+
   labs(x="pos", y = "Prop '0/1' site") +
   ggtitle("Prop '0/1' site by position") +
-  scale_x_continuous(breaks = seq(0, position_vector1[length(position_vector1)], 
+  scale_x_continuous(breaks = seq(0, position[length(position)], 
                                   by = 1000000)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
-# Het per Individual
 
-g6=ggplot()+
-  geom_point(data=Prop_Ind_geno_table1_Het, 
-             aes(y=Prop_Ind_geno_table1_Het$`colSums(geno1 == "0/1")`, 
-                 x=row.names(Prop_Ind_geno_table1_Het)),
-             color="purple",
-             size=2) +
+# Het per Individual
+g6 <- summary_individuals |>
+  ggplot()+
+  geom_point(aes(y = prop_het_pos, x = ind, color = pop), size = 1) +
   labs(x="Ind", y = "Prop '0/1' site") +
   ggtitle("Prop '0/1' site by individuals") + 
   theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100))
 
-## Save plots
 
+
+# Mean depth per position, per population : 
+position = as.numeric(str_remove(rownames(DP1), "1_"))
+DP2 <- as_tibble(DP1)
+DP2 <- DP2 |>
+  mutate(position = position) |>
+  pivot_longer(cols = 1:88, 
+               names_to = "individu", 
+               values_to = "depth") |>
+  mutate(pop = str_extract(individu, "^[A-Za-z]+|^[0-9]+")) |>
+  mutate(pop = ifelse(pop == "A", "N_ATL",
+                      ifelse(pop == "Ber", "BERING", 
+                             ifelse(pop == "Chi", "CHILI", 
+                                    ifelse(pop == "Kar", "KARAGINSKY", 
+                                           ifelse(pop == "Pe", "PEROU", "MADAGASCAR"))))))
+g7 <- DP2 |>
+  group_by(position, pop) |>
+  reframe(mean_DP = mean(depth)) |>
+  ggplot(aes(x = position, y = mean_DP, color = pop)) + 
+  geom_point(alpha = 0.3) +
+  scale_y_continuous(limits = c(0,200), breaks = seq(0, 200, by = 10))
+
+
+## Save plots
 png(paste0("OldGenomeInfosVCF/", args[1],"_",args[2],"/DistribDepth_",args[1],"_",args[2],".png"))
 print(g0)
 dev.off()
@@ -194,3 +196,35 @@ dev.off()
 png(paste0("OldGenomeInfosVCF/", args[1],"_",args[2],"/HetPerInd_",args[1],"_",args[2],".png"))
 print(g6)
 dev.off()
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
