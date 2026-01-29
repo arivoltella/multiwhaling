0e95913bcd2efe176bf66851fe1a2f520821e539
Achille Rivoltella <achille.rivoltella@gmail.com>
2026-01-29 16:37:39 +0100

    Finioler plots quality

--- quality/01_PlotQuality.R
+++ quality/01_PlotQuality.R
@@ -1,3 +1,7 @@
+#################### 27/01/26 #####################
+########### Plot the quality of data ##############
+############# Achille RIVOLTELLA ##################
+
 library(vcfR)
 library(ggplot2)
 library(reshape2)
@@ -47,11 +51,11 @@ summary_position = data.frame(depth_pos = apply(DP1, 1, mean,na.rm=T))
 position <- getPOS(VCF1)
 
 summary_position <- summary_position |>
-  mutate(position = position,                                 # Position du nucléotide 
-         sum_NA = rowSums(geno1 == "./."),                    # Somme des NA / position
-         prop_NA_ind = (sum_NA/dim(geno1)[2])*100,            # Proportion des NA / individu
-         het_pos = rowSums(geno1 == "0/1"),                   # Nb d'Hz / position
-         prop_het = (het_pos/dim(geno1)[2])*100)              # Proportion d'Hz / individu (%)
+  mutate(position = position,                                     # Position du nucléotide 
+         sum_NA_pos = rowSums(geno1 == "./."),                    # Somme des NA / position
+         prop_NA_position = (sum_NA_pos/dim(geno1)[2])*100,       # Proportion des NA / position
+         het_pos = rowSums(geno1 == "0/1"),                       # Nb d'Hz / position
+         prop_het = (het_pos/dim(geno1)[2])*100)                  # Proportion d'Hz / individu (%)
 head(summary_position)
 
 
@@ -61,12 +65,12 @@ head(summary_position)
 summary_individuals = data.frame(depth_ind = apply(DP1, 2, mean, na.rm = T))
 
 summary_individuals <- summary_individuals |>
-  mutate(ind = rownames(summary_individuals),                # Noms des individus 
-         pop = names_ind$pop,                                # Population d'appartenance
-         sum_NA = colSums(geno1=="./."),                     # Somme des NA / individu
-         prop_NA_pos = ((sum_NA/dim(geno1)[1])*100),         # Proportion de NA / position
-         het_ind = colSums(geno1=="0/1"),                    # Nb d'Hz / individu
-         prop_het_pos = ((het_ind/dim(geno1)[1])*100))       # Proportion d'Hz / position
+  mutate(ind = rownames(summary_individuals),                    # Noms des individus 
+         pop = names_ind$pop,                                    # Population d'appartenance
+         sum_NA_ind = colSums(geno1 == "./."),                   # Somme des NA / individu
+         prop_NA_ind = ((sum_NA_ind/dim(geno1)[1])*100),         # Proportion de NA / individu
+         het_ind = colSums(geno1 == "0/1"),                      # Nb d'Hz / individu
+         prop_het_pos = ((het_ind/dim(geno1)[1])*100))           # Proportion d'Hz / individu
 head(summary_individuals)
 
 
@@ -79,7 +83,7 @@ g0 <- summary_position |>
   ggplot()+
   geom_histogram(aes(x = depth_pos),
 		 binwidth = 4, fill = "steelblue", color = "black") +
-  scale_x_continuous(limits = c(0, 200), breaks = seq(0, 100, by = 10)) +
+  scale_x_continuous(limits = c(0, 100), breaks = seq(0, 100, by = 10)) +
   labs(x = "Distribution of average depth per site",
        y = "Count")
 
@@ -90,8 +94,8 @@ g1 <- summary_position |>
   geom_point(aes(y = depth_pos, x = position),
              color="black", pch=20, size=1) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
-  labs(y="mean depth", x = "PB")+
-  ggtitle("Average depth sequencing of SNPs DP1")+
+  labs(y="mean depth", x = "PB", 
+       title = "Average depth sequencing of SNPs")+
   scale_x_continuous(breaks = seq(0, 30e+06, by = 1000000)) +
   # vizualise only range of sequencing depth detph
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100))
@@ -100,10 +104,10 @@ g1 <- summary_position |>
 # Depth per Individual
 g2 <- summary_individuals |>
   ggplot() + 
-  geom_boxplot(aes(x = ind, y = depth_ind, color= pop)) +
+  geom_boxplot(aes(x = ind, y = depth_ind, color = pop)) +
   theme(axis.title.x = element_blank())+
-  labs(y="SNP sequencing depth", x = "samples") +
-  ggtitle("depth sequencing distribution of SNP filtered for qualtity and low depth") +
+  labs(y="SNP sequencing depth", x = "Individuals", 
+       title = "Mean depth sequencing per individual") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
   # vizualise only range of sequencing depth detph
   scale_y_continuous(breaks = seq(0, 260, by = 20), limits = c(0, 260))
@@ -112,34 +116,34 @@ g2 <- summary_individuals |>
 # NA per Position
 g3 <- summary_position |>
   ggplot() +
-  geom_point(aes(y = sum_NA, x = position), color="purple", size = 1)+
-  labs(x="pos", y = "Prop './.' site") +
-  ggtitle("Prop './.' site by position") +
+  geom_point(aes(y = prop_NA_position, x = position), color="purple", size = 1)+
+  labs(x = "pos", y = "Prop './.' site", 
+       title = "Prop './.' site by position") +
   scale_x_continuous(breaks = seq(0, max(position), 
-                                  by = 1000000)) +
+                                  by = 10000000)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 
-# NA per Individual
+# NA prop per Individual
 g4 <- summary_individuals |>
-  ggplot()+
-  geom_point(aes(y = sum_NA, x = ind), color="purple", size=2) +
-  labs(x="Ind", y = "Prop './.' site") +
-  ggtitle("Prop './.' site by individuals") + 
+  ggplot() +
+  geom_point(aes(y = prop_NA_ind, x = ind), color="purple", size=2) +
+  labs(x="Ind", y = "Prop './.' site", 
+       title = "Prop './.' site by individuals") +
   theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1)) +
-  scale_y_continuous(breaks = seq(0, 10000, by = 1000), limits = c(0,max(summary_individuals$sum_NA)))
+  scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0, 100))
 
 
 # Het per Position
 g5 <- summary_position |>
   ggplot() +
-  geom_point(aes(y = het_pos , x= position), color = "purple", size = 0.3)+
-  labs(x="pos", y = "Prop '0/1' site") +
-  ggtitle("Prop '0/1' site by position") +
+  geom_point(aes(y = prop_het , x = position), color = "purple", size = 0.3)+
+  labs(x = "pos", y = "Prop '0/1' site", 
+       title = "Prop '0/1' site by position") +
   scale_x_continuous(breaks = seq(0, max(position), 
-                                  by = 1000000)) +
-  scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100)) +
+                                  by = 10000000)) +
+  scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0, 100)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 
@@ -147,8 +151,8 @@ g5 <- summary_position |>
 g6 <- summary_individuals |>
   ggplot()+
   geom_point(aes(y = prop_het_pos, x = ind, color = pop), size = 1) +
-  labs(x="Ind", y = "Prop '0/1' site") +
-  ggtitle("Prop '0/1' site by individuals") + 
+  labs(x="Ind", y = "Prop '0/1' site",
+       title = "Prop '0/1' site by individuals") +
   theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), limits = c(0,100))
 
@@ -181,28 +185,28 @@ g7 <- DP2 |>
 
 
 ## Save plots
-png(paste0("plot/Quality/DistribDepth.png"), width = 500, height = 500)
+png(paste0("plot/Quality/DistribDepth.png"), width = 750, height = 750)
 print(g0)
 dev.off()
-png(paste0("plot/Quality/DepthPerSite.png"), width = 500, height = 500)
+png(paste0("plot/Quality/DepthPerSite.png"), width = 750, height = 750)
 print(g1)
 dev.off()
-png(paste0("plot/Quality/DepthPerInd.png"), width = 500, height = 500)
+png(paste0("plot/Quality/DepthPerInd.png"), width = 750, height = 750)
 print(g2)
 dev.off()
-png(paste0("plot/Quality/NAPerSite.png"), width = 500, height = 500)
+png(paste0("plot/Quality/NAPerSite.png"), width = 750, height = 750)
 print(g3)
 dev.off()
-png(paste0("plot/Quality/NAPerInd.png"), width = 500, height = 500)
+png(paste0("plot/Quality/NAPerInd.png"), width = 750, height = 750)
 print(g4)
 dev.off()
-png(paste0("plot/Quality/HetPerSite.png"), width = 500, height = 500)
+png(paste0("plot/Quality/HetPerSite.png"), width = 750, height = 750)
 print(g5)
 dev.off()
-png(paste0("plot/Quality/HetPerInd.png"), width = 500, height = 500)
+png(paste0("plot/Quality/HetPerInd.png"), width = 750, height = 750)
 print(g6)
 dev.off()
-png(paste0("plot/Quality/depth_by_pop.png"), width = 500, height = 500)
+png(paste0("plot/Quality/depth_by_pop.png"), width = 750, height = 750)
 print(g7)
 dev.off()
 
