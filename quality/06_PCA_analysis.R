e614ff5bd35203acf4a4e05f125111dc5f0ca0d5
Achille Rivoltella <achille.rivoltella@gmail.com>
2026-02-20 14:57:49 +0100

    chgt noms LD.5

--- quality/06_PCA_analysis.R
+++ quality/06_PCA_analysis.R
@@ -43,31 +43,31 @@ snps_tot <- nrow(genotype)
 qqq <- apply(genotype, 1, fun_geno_allele)
 genotype_num <- matrix(as.numeric(qqq), ncol = n_ind, nrow = snps_tot, byrow = T)
 
-write.table(genotype_num, file = "/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.geno", quote = F, 
+write.table(genotype_num, file = "/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.5.geno", quote = F, 
             sep = "", row.names = F, col.names=F)
 
 #### Faire la PCA
-whale_pca <- pca(input.file = "/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.geno")
+whale_pca <- pca(input.file = "/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.5.geno")
 summary(whale_pca)
 
 # Eigenvalues : 
-pdf("/shared/projects/multiwhaling/multiwhaling/plot/PCA/eigenvalues_all_pop_maf_LD_pruned.pdf")
+pdf("/shared/projects/multiwhaling/multiwhaling/plot/PCA/eigenvalues_all_pop_maf_LD_pruned.5.pdf")
 plot(whale_pca, lwd = 5, col = "blue", cex = .7, xlab = ("Factors"), ylab = "Eigenvalues")
 dev.off()
 
 # On récupère les données dans un nouveau dossier généré automatiquement par la fonction 'pca' utilisée
-eigen_pca <- read.table("/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.pca/VCF_maf_LD_Pruned.eigenvalues", header = F)
+eigen_pca <- read.table("/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.5.pca/VCF_maf_LD_Pruned.5.eigenvalues", header = F)
 eigen_pca <- (eigen_pca / ((sum(eigen_pca)))) * 100
 eigen_pca <- unlist(round(eigen_pca, digits = 2))
 
 # Récupération des coordonnées de chaque individu dans les projections : 
-proj <- read.table("/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.pca/VCF_maf_LD_Pruned.projections", header = F)
+proj <- read.table("/shared/projects/multiwhaling/multiwhaling/data/VCF_maf_LD_Pruned.5.pca/VCF_maf_LD_Pruned.5.projections", header = F)
 proj <- as_tibble(proj) |>
   dplyr::select(1:4) |>
   mutate(pop = names_ind |> arrange(Population))
-saveRDS(proj, "/shared/projects/multiwhaling/multiwhaling/plot/PCA/projections_maf_LD_Pruned.RDS")
+saveRDS(proj, "/shared/projects/multiwhaling/multiwhaling/plot/PCA/projections_maf_LD_Pruned.5.RDS")
 
-pdf(paste("/shared/projects/multiwhaling/multiwhaling/plot/PCA/PCA_all_pop_maf_LD_Pruned.pdf"))
+pdf(paste("/shared/projects/multiwhaling/multiwhaling/plot/PCA/PCA_all_pop_maf_LD_Pruned.5.pdf"))
 proj |>
   ggplot(aes(x = V1, y = V2, color = pop$Population)) + 
   geom_point(size = 2) + 
