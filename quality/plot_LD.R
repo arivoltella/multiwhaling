19bdf57562219e517b5831b9cf0473d90961c931
Achille Rivoltella <achille.rivoltella@gmail.com>
2026-02-24 15:25:09 +0100

    filtrer 1 SNP tous les 100

--- quality/plot_LD.R
+++ quality/plot_LD.R
@@ -11,11 +11,18 @@ VCF1 <- read.vcfR("/shared/projects/multiwhaling/Achille/VCF/sample_SNP.vcf.gz")
 list_pop <- readRDS("/shared/projects/multiwhaling/multiwhaling/data/list_pop.RDS")
 
 # Enlever les 2 individus apparentés : 
-VCF1 <- VCF1[,c("FORMAT", unlist(list_pop))]   
+VCF1 <- VCF1[,c("FORMAT", unlist(list_pop))]
+pos <- getPOS(VCF1)
+
+# Et on ne garde qu'un SNP tous les 100 pour diminuer temps de calcul : 
+pos_matrix <- seq(1,length(pos),100)
+
+VCF_to_LDheatmap <- VCF1[pos_matrix,]
+
 
 #### Visualiser le déséquilibre de liaison sur une heatmap : 
-snpMatrix <- vcfR2SnpMatrix(VCF1)
-pos <- snpMatrix$genetic.distances
+snpMatrix <- vcfR2SnpMatrix(VCF_to_LDheatmap)
+pos_LDhm <- snpMatrix$genetic.distances
 
 # On modifie le nom des colonnes pour que les SNP aient des noms : 
 colnames(snpMatrix$data) <- paste0("SNP_", seq_len(ncol(snpMatrix$data)))
@@ -24,10 +31,10 @@ print("Type de l'objet que l'on donne à LD_heatmap")
 str(snpMatrix$data)
 
 # Sauvegarde du plot : 
-png("/shared/projects/multiwhaling/multiwhaling/plot/LD_heatmap/LD_chr9.png")
+png("LD_chr9.png")
 LDheatmap(snpMatrix$data, 
-          genetic.distances = pos, 
-          color = colorRampPalette(c("red","white"))(20), 
+          genetic.distances = pos_LDhm, 
+          color = colorRampPalette(c("darkred","red", "white"))(20), 
           flip = T)
 dev.off()
 
