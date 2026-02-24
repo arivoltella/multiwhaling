a517da15297acd7fcbb5064f714a3a892363376a
Achille Rivoltella <achille.rivoltella@gmail.com>
2026-02-24 12:36:16 +0100

    debug LD plot

--- quality/plot_LD.R
+++ quality/plot_LD.R
@@ -7,7 +7,7 @@ library(vcfR)
 library(LDheatmap)
 
 # Import data : 
-VCF1 <- read.vcfR("/shared/projects/multiwhaling/Achille/VCF/HumpbackTot_9_GATK_TAG_Flowqual_Noindels_Norepeat_SNP.vcf.gz")
+VCF1 <- read.vcfR("/shared/projects/multiwhaling/multiwhaling/data/VCF_filtered.vcf.gz")
 list_pop <- readRDS("/shared/projects/multiwhaling/multiwhaling/data/list_pop.RDS")
 
 # Enlever les 2 individus apparentés : 
@@ -15,8 +15,17 @@ VCF1 <- VCF1[,c("FORMAT", unlist(list_pop))]
 
 # Visualiser le déséquilibre de liaison sur une heatmap : 
 snpMatrix <- vcfR2SnpMatrix(VCF1)
+
+print("Type de l'objet que l'on donne à LD_heatmap")
+str(snpMatrix$data)
+
 pos <- snpMatrix$genetic.distances
 
+# On modifie le nom des colonnes pour que les SNP aient des noms : 
+colnames(snpMatrix$data) <- paste0("SNP_", seq_len(ncol(snpMatrix$data)))
+
+
+# Sauvegarde du plot : 
 png("/shared/projects/multiwhaling/multiwhaling/plot/LD_heatmap/LD_chr9.png")
 LDheatmap(snpMatrix$data, 
           genetic.distances = pos, 
